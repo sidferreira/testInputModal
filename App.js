ae76c4d20073855aaebd68c5d80f6396c85da17a
Sid Ferreira <143615+sidferreira@users.noreply.github.com>
2021-11-18 16:52:18 -0800

    da bug

--- App.js
+++ App.js
@@ -7,26 +7,22 @@
  */
 
 import React from 'react';
-import type {Node} from 'react';
+import Modal from 'react-native-modal';
 import {
   SafeAreaView,
   ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
   useColorScheme,
   View,
+  TextInput,
+  Keyboard,
+  Platform,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
+const Section = ({children, title}) => {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
@@ -52,39 +48,56 @@ const Section = ({children, title}): Node => {
   );
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
+  const inputRef = React.useRef();
+  const [showModal, setShowModal] = React.useState(true);
+  const [showInput, setShowInput] = React.useState(false);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  React.useEffect(() => {
+    if (showInput && inputRef.current) {
+      setTimeout(() => {
+        inputRef.current.focus();
+      }, 30);
+    }
+  }, [showInput]);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView>
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <View style={{flex: 1}}>
+          <View style={{borderWidth: 30, borderColor: 'green'}}>
+            {showInput ? (
+              <TextInput
+                ref={inputRef}
+                autoFocus={Platform.select({ios: true, android: undefined})}
+                style={{borderWidth: 1, borderColor: 'red'}}
+              />
+            ) : null}
+            {!showModal ? (
+              <Text
+                onPress={() => {
+                  setShowModal(true);
+                  setShowInput(false);
+                }}>
+                Press to Open Modal
+              </Text>
+            ) : null}
+          </View>
         </View>
+        <Modal
+          isVisible={showModal}
+          onModalHide={() => {
+            setShowInput(true);
+          }}>
+          <View style={{flex: 1, backgroundColor: 'white', padding: 30}}>
+            <Text
+              onPress={() => {
+                setShowModal(false);
+              }}>
+              Press to Edit
+            </Text>
+          </View>
+        </Modal>
       </ScrollView>
     </SafeAreaView>
   );
